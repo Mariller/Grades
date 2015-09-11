e50f217498a0639dcbf370a0c91e42e94cce19c0
Mariller <daadvent@gmail.com>
2015-09-11 16:30:00 -0600

    Completed basic version of program!

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -5,11 +5,39 @@ namespace Grades
 {
     class GradeBook
     {
-        public void AddGrade(float grade)
+        public GradeBook()
         {
-            grades.Add(grade);
+            grades = new List<float>();
         }
 
-        List<float> grades = new List<float>();
+        public void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0f;
+
+            foreach( float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+            
+            return stats;
+        }
+
+        private List<float> grades;
+
+       
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -8,8 +8,19 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            
+
             GradeBook book = new GradeBook();
             book.AddGrade(91);
+            book.AddGrade(89.1f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine("Average: " + stats.AverageGrade);
+            Console.WriteLine("Highest: " + stats.HighestGrade);
+            Console.WriteLine("Lowest: " + stats.LowestGrade);
+
         }
     }
 }
